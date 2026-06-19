e3a84d7e6c9ae194179c7bd3ce360427a0e1d0bd
christofel17 <christofel.pardede@gmail.com>
2026-06-19 22:06:38 +0700

    Initial commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Henry on 19/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
