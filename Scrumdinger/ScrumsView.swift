2fdc86f943ac702916d0244d18df5c7a96fdd275
christofel17 <christofel.pardede@gmail.com>
2026-06-20 13:18:03 +0700

    Display list of daily scrum

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Henry on 20/06/26.
+//
+
+import Foundation
+import SwiftUI
+import ThemeKit
+
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
