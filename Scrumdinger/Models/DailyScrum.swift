9ceace09dbccde196589ea2819db616bac7546fd
christofel17 <christofel.pardede@gmail.com>
2026-06-20 11:55:27 +0700

    Import ThemeKit & add sample data

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,16 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Henry on 20/06/26.
+//
+
+import Foundation
+import ThemeKit
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
