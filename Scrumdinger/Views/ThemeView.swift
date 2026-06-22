91b63913ba83838ede7734062c923d85b2428d05
christofel17 <christofel.pardede@gmail.com>
2026-06-22 16:43:29 +0700

    Add theme view

---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,27 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Henry on 22/06/26.
+//
+
+import SwiftUI
+import ThemeKit
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+
+#Preview {
+    ThemeView(theme: .buttercup)
+}
